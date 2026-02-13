481091d932891cea190476c7a5d74fae23d9af10
Isabelle Eriksson <isabelleeriksson13@gmail.com>
2026-02-12 19:59:28 -0800

    added component assets

--- Assets/Scripts/ComponentScipts/RobotCore.cs
+++ Assets/Scripts/ComponentScipts/RobotCore.cs
@@ -5,6 +5,14 @@ public class RobotCore : RobotComponent
 {
     private List<RobotComponent> _components = new List<RobotComponent>();
 
+    public void AddComponent(RobotComponent component)
+    {
+        _components.Add(component);
+    }
+    public void RemoveComponent(RobotComponent component)
+    {
+        _components.Remove(component);
+    }
     public override void DestroySelf()
     {
         Debug.Log("Core cannot be deleted");
--- Assets/Scripts/GridScripts/GridManager.cs
+++ Assets/Scripts/GridScripts/GridManager.cs
@@ -6,10 +6,14 @@ public class GridManager : MonoBehaviour
 {
     [SerializeField] private GridData _gridData;
     [SerializeField] private GridBuilder _gridBuilder;
+    private RobotCore _robotCore;
 
     private void Awake()
     {
         _gridData.Initialize();
         _gridBuilder.Build(_gridData);
+        _robotCore = gameObject.AddComponent<RobotCore>();
+        
+        _gridData.Add(3, 1, _robotCore);
     }
 }
