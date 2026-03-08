93116c61df2cebf99dee26fb1f08aa3c7b252aae
Isabelle Eriksson <isabelleeriksson13@gmail.com>
2026-03-08 03:20:06 -0700

    Basic UI + SceneChange script

--- Assets/Scripts/ChangeScenes.cs
+++ Assets/Scripts/ChangeScenes.cs
@@ -5,8 +5,10 @@ using UnityEngine.SceneManagement;
 
 public class ChangeScenes : MonoBehaviour
 {
-    public void GoToLevelMenu()
+    [SerializeField] private string _sceneName;
+
+    public void LoadScene()
     {
-        SceneManager.LoadScene("LevelMenu");
+        SceneManager.LoadScene(_sceneName);
     }
 }
